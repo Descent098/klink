0d219831c05a7359c984739f2542a5acb3383747
Kieran Wood <kieran@canadiancoding.ca>
2025-04-07 21:58:38 -0600

    Killing prefetch fully

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://klnk.ca",
-    prefetch: {
-        prefetchAll: false,
-        defaultStrategy: 'viewport'
-      }
+    prefetch: false
 });
